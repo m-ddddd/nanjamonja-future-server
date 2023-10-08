0b4da21aca76970bc52446776bdf50a7f0fb7639
m-ddddd <d.miyakawa.md@gamil.com>
2023-10-08 12:11:31 +0900

    DB作成

--- nanjamonja/api/models.py
+++ nanjamonja/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    user_name = models.CharField(max_length=100)
+    highest_score = models.IntegerField(default=0)
+    participant_count = models.IntegerField(default=0)
+
+
+class Character(models.Model):
+    content = models.BinaryField(max_length=None)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Parts(models.Model):
+    content = models.BinaryField(max_length=None)
+    parts_type = models.IntegerField()
