84a849129c0e9cb0f2c0cd9213c0a243796a38f0
m-ddddd <d.miyakawa.md@gamil.com>
2023-10-08 15:57:28 +0900

    デフォルトフラグ追加　 content の型をbinary→textに変更

--- nanjamonja/api/models.py
+++ nanjamonja/api/models.py
@@ -5,10 +5,11 @@ class User(models.Model):
     user_name = models.CharField(max_length=100)
     highest_score = models.IntegerField(default=0)
     participant_count = models.IntegerField(default=0)
+    default_flg = models.IntegerField(default=0)
 
 
 class Character(models.Model):
-    content = models.BinaryField(max_length=None)
+    content = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
--- nanjamonja/api/views.py
+++ nanjamonja/api/views.py
@@ -9,16 +9,16 @@ def user_insert(request):
         user_name=request.data['user_name']
     )
     Character.objects.create(
-        content=bytes(request.data['content'], 'utf-8'),
+        content=request.data['content'],
         user=user_info
     )
     print(request.data)
-    return Response({"message": "insertです"})
+    return Response({'result': 'sucsses'})
 
 
 @api_view(['GET'])
 def user_get(request):
-    users = User.objects.all()
+    users = User.objects.all().order_by('-id')
     datas = []
     for row in users:
         char = Character.objects.filter(user=row.id)
@@ -26,7 +26,8 @@ def user_get(request):
             {'user_name': row.user_name},
             {'content': char[0].content},
             {'highest_score': row.highest_score},
-            {'participant_count': row.participant_count}
+            {'participant_count': row.participant_count},
+            {'default_flg': row.default_flg},
         ]
         datas.append(data)
 
@@ -35,4 +36,15 @@ def user_get(request):
 
 @api_view(['POST'])
 def user_update(request):
-    return Response({"message": "updです"})
+    for user_info in request.data:
+        user_id = user_info.get('id')
+        score = user_info.get('score')
+        if isinstance(score, str):
+            score = int(score)
+        user = User.objects.get(id=user_id)
+        if score > user.highest_score:
+            user.highest_score = score
+        user.participant_count += 1
+        user.save()
+
+    return Response({'result': 'sucsses'})
