8e30c5bb1ed2d69c10404ee2285692fbefd6f7d7
m-ddddd <d.miyakawa.md@gamil.com>
2023-10-08 12:46:36 +0900

    sample

--- nanjamonja/api/views.py
+++ nanjamonja/api/views.py
@@ -1,15 +1,26 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from api.models import Character, User
 
 
 @api_view(['POST'])
 def user_insert(request):
+    Character.objects.create(
+
+    )
+    print(request.data)
     return Response({"message": "insertです"})
 
 
 @api_view(['GET'])
 def user_get(request):
-    return Response({"message": "getです"})
+    hoge = [
+        {'test': 'a'},
+        {'test': 'a'},
+        {'test': 'a'},
+        {'test': 'a'}
+    ]
+    return Response(hoge)
 
 
 @api_view(['POST'])
