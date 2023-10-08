b0e20980b737d36c616b3676e74ee5af07e825c8
m-ddddd <d.miyakawa.md@gamil.com>
2023-10-08 14:43:52 +0900

    仮作成

--- nanjamonja/api/views.py
+++ nanjamonja/api/views.py
@@ -5,8 +5,12 @@ from api.models import Character, User
 
 @api_view(['POST'])
 def user_insert(request):
+    user_info = User.objects.create(
+        user_name=request.data['user_name']
+    )
     Character.objects.create(
-
+        content=bytes(request.data['content'], 'utf-8'),
+        user=user_info
     )
     print(request.data)
     return Response({"message": "insertです"})
@@ -14,13 +18,19 @@ def user_insert(request):
 
 @api_view(['GET'])
 def user_get(request):
-    hoge = [
-        {'test': 'a'},
-        {'test': 'a'},
-        {'test': 'a'},
-        {'test': 'a'}
-    ]
-    return Response(hoge)
+    users = User.objects.all()
+    datas = []
+    for row in users:
+        char = Character.objects.filter(user=row.id)
+        data = [
+            {'user_name': row.user_name},
+            {'content': char[0].content},
+            {'highest_score': row.highest_score},
+            {'participant_count': row.participant_count}
+        ]
+        datas.append(data)
+
+    return Response(datas)
 
 
 @api_view(['POST'])
